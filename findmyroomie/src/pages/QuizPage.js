b804f556ddacace5b5d6960609c860d9fee90c6b
Say-young <seyoung990525@gmail.com>
2021-02-08 23:34:10 +0900

    질문지 DB 업데이트

--- findmyroomie/src/pages/QuizPage.js
+++ findmyroomie/src/pages/QuizPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ButtonComponent from '../components/ButtonComponent';
+import contents from './sections/questions';
 
 const Container = styled.div`
     display: ${props => props.isShow === true ? 'flex' : 'none'};
@@ -22,69 +23,6 @@ function QuizPage({ isShow }) {
 
     const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 4));
 
-    const contents = [
-        {
-            'question': '친구와의 약속이 취소되었을 때, 당신은?',
-            'answers': [
-                {
-                    text: '나가기 귀찮았는데 잘 됐다!',
-                    score: ''
-
-                },
-                {
-                    text: '다른 친구를 찾아봐야지',
-                    score: ''
-                }
-            ]
-        },
-        {
-            'question': '시험기간 중 가장 좋아하는 공부 장소는?',
-            'answers': [
-                {
-                    text: '편한게 최고지! 내 방',
-                    score: ''
-
-                },
-                {
-                    text: '멀리 가긴 귀찮아.. 기숙사 내 스터디룸',
-                    score: ''
-                },
-                {
-                    text: 'ecc 열람실 셀프 감금',
-                    score: ''
-                }
-            ]
-        },
-        {
-            'question': '문제3',
-            'answers': [
-                {
-                    text: '보기1',
-                    score: ''
-
-                },
-                {
-                    text: '보기2',
-                    score: ''
-                }
-            ]
-        },
-        {
-            'question': '문제4',
-            'answers': [
-                {
-                    text: '보기1',
-                    score: ''
-
-                },
-                {
-                    text: '보기2',
-                    score: ''
-                }
-            ]
-        },
-
-    ]
 
     //중복제거필요
     const onClickNextBtn = () => {
@@ -95,6 +33,7 @@ function QuizPage({ isShow }) {
     return (
         <Container isShow={isShow}>
             <Text>{contents[randomNum].question}</Text>
+
             {contents[randomNum].answers.map((answer, i) => (
                 <ButtonComponent
                     key={'answer-' + i}
---
+++ findmyroomie/src/pages/sections/results.js
@@ -0,0 +1,8 @@
+export const results = [
+    {
+        'name': '유형이름',
+        'explain': '설명',
+        'best': '잘맞는유형을어떻게넣어야되려나',
+        'worst': '안맞는유형'
+    }
+]
