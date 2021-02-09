ec77356b2e1205a4d849a4c975313df16d63e990
Say-young <seyoung990525@gmail.com>
2021-02-09 21:46:46 +0900

    결과페이지 조합 정보 추가

--- findmyroomie/src/pages/ResultPage.js
+++ findmyroomie/src/pages/ResultPage.js
@@ -4,21 +4,54 @@ import results from './sections/results';
 
 const Wrapper = styled.div`
     display: ${props => props.isShow === true ? 'flex' : 'none'};
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+`
+
+//아래는 아직 안정함 레이아웃만 나눠놓음
+const A = styled.div`
+    width:300;
+    border : 1px solid black;
+`
+
+const B = styled.div`
+    width:150;
+    border : 1px solid black;
+`
+
+const C = styled.div`
+    width:150;
+    border : 1px solid black;
+`
+
+const D = styled.div`
+    display: ${props => props.isShow === 16 ? 'none' : 'flex'};
 `
 
 function ResultPage({ isShow, finalNum }) {
 
     //퍼블리싱 어떻게 할지 생각해보기
-    //잘맞는 룸미 안맞는 룸미에 대한 desc랑 img도 출력해줘야 되지 않을까?
 
     return (
         <Wrapper isShow={isShow}>
-            {results[finalNum].sub}
-            {results[finalNum].result}
-            {results[finalNum].img}
-            {results[finalNum].desc}
-            {results[finalNum].GoodRoomie}
-            {results[finalNum].BadRoomie}
+            <A>
+                {results[finalNum].sub}
+                {results[finalNum].result}
+                {results[finalNum].img}
+                {results[finalNum].desc}
+            </A>
+            <D isShow={finalNum}>
+                <B>
+                    {results[results[finalNum].GoodRoomie].sub}
+                    {results[results[finalNum].GoodRoomie].result}
+                </B>
+
+                <C>
+                    {results[results[finalNum].BadRoomie].sub}
+                    {results[results[finalNum].BadRoomie].result}
+                </C>
+            </D>
         </Wrapper>
     );
 }
